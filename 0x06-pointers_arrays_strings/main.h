364754752eb76092d2b39e6a3655c544d221203f
Mai2aa <mysaadil0@gmail.com>
2023-08-24 18:06:05 -0700

    0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#ifnde MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -9,4 +9,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-#endif MAIN_H
+#endif
