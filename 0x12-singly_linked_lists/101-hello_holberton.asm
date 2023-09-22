07464cee45e5152a0f1a613194c1ce133be5aa71
Mai2aa <mysaadil0@gmail.com>
2023-09-21 19:45:37 -0700

    advanced

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,16 @@
-	global main
-	extern printf
+section .data
+    msg db "Hello, Holberton",10      ; 10 is the ASCII code for a new line (LF)
+
+section .text
+    global main
+
 main:
-	mov edi, format
-	xor eax, eax
-	call printf
-	mov eax, 0
-	ret
-format:	db "Hello, Holberton", 10
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 17
+    syscall
+    
+    mov rax, 60
+    mov rdi, 0
+    syscall
