151e5148d60fd16bc0565ee012c00ff885fbac9d
Mai2aa <mysaadil0@gmail.com>
2023-09-28 19:58:57 -0700

    tasks

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,9 +5,24 @@
  * @n: the number to be converted
  * Retrun: void
  */
+unsigned long int mask;
+int zeros;
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n / 2);
-	printf("%lu", n % 2);
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+	mask = 1UL << (sizeof(n) * 8 - 1);
+	zeros = 1;
+
+	while (mask != 0)
+	{
+		if ((n & mask) != 0)
+			zeros = 0;
+		if (!zeros)
+			printf("%d", (n & mask ) != 0);
+		mask >>=1;
+	}
 }
