0107d276910de410f16c8c547b83bc956effcdd3
Mai2aa <mysaadil0@gmail.com>
2023-09-29 18:10:52 -0700

    advanced

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if big and 1 if little
+ */
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	char *ptr = (char*)&num;
+
+	if (*ptr)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,4 +8,5 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _putchar(char c);
 unsigned long int mask;
+int get_endianness(void);
 #endif
