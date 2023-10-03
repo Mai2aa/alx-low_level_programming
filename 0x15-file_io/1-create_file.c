e8bbf7bc54f5ece7dca0d874dca9c791c87d6f8e
Mai2aa <mysaadil0@gmail.com>
2023-10-02 19:19:19 -0700

    tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <unistd.h>
+#include <stdlib.h>
 /**
  * create_file - creates a file
  * @filename: pointer to thname of file
@@ -9,11 +10,12 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file, w, len = 0;
+
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++)
+		for (len = 0; text_content[len];)
 			len++;
 	}
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
