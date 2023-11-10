6e5306d82f5d3294084c462911303a7a0f28f5ec
Mai2aa <mysaadil0@gmail.com>
2023-11-10 04:29:53 +0200

    tak 3

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t count = 0;
+size_t count = 0;
 
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
-    return count;
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
+return count;
 }
--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,2 +1,34 @@
 #include "lists.h"
+/**
+ * add_dnodeint_end - adds a node at the end
+ * @head: pointer to the head of the list
+ * @n: node to be added
+ * Return: the address of the new node or NULL if fail
+*/
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+dlistint_t *newNode = malloc(sizeof(dlistint_t));
+if (newNode == NULL)
+{
+return NULL;
+}
+newNode->n = n;
+newNode->next = NULL;
 
+if (*head == NULL)
+{
+newNode->prev = NULL;
+*head = newNode;
+} 
+else
+{
+dlistint_t *temp = *head;
+while (temp->next != NULL)
+{
+temp = temp->next;
+}
+temp->next = newNode;
+newNode->prev = temp;
+}
+return newNode;
+}
