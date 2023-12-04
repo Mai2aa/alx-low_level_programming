9eacd2a43c404b99e35947ad5733c29153016d6d
Mai2aa <mysaadil0@gmail.com>
2023-12-03 18:43:20 -0800

    task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,21 +9,22 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *current = ht->array[index];
+	unsigned long int index;
+	hash_node_t *current;
 
-	if (key == NULL || ht == NULL)
+	if (ht == NULL || key == NULL)
 	{
 		return (NULL);
 	}
-	
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
 			return (current->value);
 		}
-		
+
 		current = current->next;
 	}
 
