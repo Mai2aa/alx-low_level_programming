ad21e86416c4f03d061bc07d64a444d2bebb6696
Mai2aa <mysaadil0@gmail.com>
2024-05-08 14:41:38 -0700

    task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-#include <search_algos.h>
+#include "search_algos.h"
 /**
  * linear_search - goes through the whole array
- * @array: pointer to an array 
+ * @array: pointer to an array
  * @size: size of an array
  * @value: the value searched for
+ * Return: a value on Successs, -1 if failed
  */
 int linear_search(int *array, size_t size, int value)
 {
-int i = 0;
+size_t i = 0;
 if (array == NULL)
 {
 return (-1);
 }
-while (i < int(size))
+while (i < size)
 {
-printf("Value checked array[%d] = [%d]\n", i, array[i]);
+printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 if (array[i] == value)
 {
 return (i);
